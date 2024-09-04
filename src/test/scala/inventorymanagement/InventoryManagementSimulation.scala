480667dab26b2f4a18f254beb12ac8d31470c661
Sam McConnell <sam.mcconnell@improving.com>
2024-09-04 11:52:04 -0700

    1 minute test at 10 000 requests per second

--- src/test/scala/inventorymanagement/InventoryManagementSimulation.scala
+++ src/test/scala/inventorymanagement/InventoryManagementSimulation.scala
@@ -96,7 +96,7 @@ class InventoryManagementSimulation extends Simulation {
     )
 
   // @@@@@ THIS IS WHERE YOU DEFINE THE RATE AND DURATION OF THE TEST @@@@@
-  setUp(request.inject(constantUsersPerSec(1000).during(30.seconds)))
+  setUp(request.inject(constantUsersPerSec(10000).during(60.seconds)))
     .protocols(httpProtocol)
 
   private def generateAddRequestBody: String = {
